9c22ec2182a11423731ce316a65e21758cdf5e30
BrunoLeao7 <blbruno0712@gmail.com>
2022-09-30 20:38:35 -0300

    v0.2

--- Progs.c
+++ Progs.c
@@ -5,6 +5,29 @@
 int main(void)
 {
 
+    int prog;
+
+    printf("Escolha o programa de 1 a ...: ");
+    scanf("%d", &prog);
+
+    switch (prog)
+    {
+        case 1:
+        {
+            p1();
+        }
+
+        case 2:
+        {
+            p2();            
+        }
+
+    }   
+}
+
+void p1() 
+{
+
     double av1, av2, media;
 
     printf("Escreva a nota da AV1: ");
@@ -17,4 +40,25 @@ int main(void)
     printf("Media do aluno: %.1lf\n", media);
 
     system("pause");
+
+}
+
+void p2()
+{
+
+    int a, b, c, d, pontos;
+
+    printf("Digite o valor A: ");
+    scanf("%d", &a);
+    printf("Digite o valor B: ");
+    scanf("%d", &b);
+    printf("Digite o valor C: ");
+    scanf("%d", &c);
+    printf("Digite o valor D: ");
+    scanf("%d", &d);
+
+    pontos = (a * b - c * d);
+
+    printf("Pontos: %d", pontos);
+
 }
