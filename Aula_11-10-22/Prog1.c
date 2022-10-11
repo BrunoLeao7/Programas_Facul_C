0851ca920471b4eb51177ddeb36b20fcca5d98ff
BrunoLeao7 <blbruno0712@gmail.com>
2022-10-11 20:25:39 -0300

    Tabuada v0.2

--- Aula_11-10-22/Prog1.c
+++ Aula_11-10-22/Prog1.c
@@ -3,13 +3,16 @@
 
 int main(void) {
 
-    int valor, i, result;
+    int valor, i, result, final;
     i=1;
 
     printf("Digite o valor da tabuada: ");
     scanf("%d",&valor);
 
-    while(i <= 10) {
+    printf("Digite o final da tabuada: ");
+    scanf("%d",&final);
+
+    while(i <= final) {
 
         result = valor*i;
         printf("%d x %d = %d\n",valor, i, result);
