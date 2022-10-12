476aaed5efac67159e1dbe97bd61ecff595f7bae
BrunoLeao7 <blbruno0712@gmail.com>
2022-10-11 22:22:58 -0300

    Cronometro v0.1

--- Aula_11-10-22/Prog1.c
+++ Aula_11-10-22/Prog1.c
@@ -1,24 +1,31 @@
-#include <stdlib.h>;
-#include <stdio.h>;
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
 
-    int valor, i, result, final;
-    i=1;
+    int min=0, seg=0, ciclos=0, total;
 
-    printf("Digite o valor da tabuada: ");
-    scanf("%d",&valor);
+    printf("Digite quantos minutos deseja contar: ");
+    scanf("%d", &total);
+    total = total*60;
 
-    printf("Digite o final da tabuada: ");
-    scanf("%d",&final);
+    while(ciclos <= total) {
 
-    while(i <= final) {
+        printf("%02d:%02d\n", min, seg);
+        seg++;
 
-        result = valor*i;
-        printf("%d x %d = %d\n",valor, i, result);
-        i++;
+        if (seg%60 == 0) {
+            
+           min++;
+           seg = 0;
+        }
+        
+        sleep(1);
+        system("cls");
+        ciclos++;
 
     }
-
+    printf("Acabou seu tempo!");
     return 0;
 }
